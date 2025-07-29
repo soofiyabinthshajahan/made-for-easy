2c117b0f8568b400612391b640f41faf3158dff2
soofiyabinthshajahan <soofiyashajahan@gmail.com>
2025-07-29 10:58:09 +0530

    feat: Add top header component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-  background: #ffffff;
-  color: #0057b7;
-  padding: 2rem;
-  text-align: center;
-`;
-
-const Title = styled.h1`
-  font-size: 3rem;
-  font-weight: bold;
-  margin-bottom: 1rem;
-`;
-
-const Subtitle = styled.p`
-  font-size: 1.5rem;
-  margin-bottom: 1.5rem;
-`;
-
-const Note = styled.div`
-  background: #e0f0ff;
-  color: #004d99;
-  padding: 0.75rem 1.25rem;
-  border-radius: 9999px;
-  font-size: 0.9rem;
-`;
-
+import React from 'react'
+import HeaderTop from './Components/Header/HeaderTop'
+import './Global.css'
 function App() {
   return (
-    <Wrapper>
-      <Title>Welcome to MadeForEasy</Title>
-      <Subtitle>Inshallah We will be officially starting on <strong>July 7</strong>!</Subtitle>
-      <Note>Stay tuned — simplicity in healthcare is on its way.</Note>
-    </Wrapper>
-  );
+    <div>
+      <HeaderTop />
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/Components/Header/Header.styles.tsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const Header1 = styled.div`
+  width: 100%;
+  height: 10vh;
+  background-color: #b7c0cbad;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+export const HeaderContact = styled.div`
+  width: 50%;
+  height: 10vh;
+  display: flex;
+  align-items: center;
+  padding-left: 25px;
+
+  span {
+    width: 27;
+    height: 39;
+    top: 24px;
+    left: 257px;
+    opacity: 1;
+  }
+`;
+export const HeaderText = styled.p`
+  margin: 0 15px 0 5px;
+  font-size: 16px;
+  font: Inter;
+`;
+
+export const HeaderSocial = styled.div`
+  width: 50%;
+  height: 10vh;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding-right: 100px;
+
+  span {
+    width: 36.824249267578125px;
+    height: 36.999996185302734;
+    color: #313d4a;
+    top: 22px;
+    left: 1098px;
+    opacity: 1;
+  }
+`;
