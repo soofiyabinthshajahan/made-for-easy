f74b386b7d9b73f7223c3a937f58077c1ac30ea6
Travel023 <eldhoseer16@gmail.comgit config --global user.email eldhoseer16@gmail.comgit config --global user.name Eldhose>
2025-08-08 19:08:42 +0530

    feat: add complete responsive layout for all sections - Implement fully responsive home page - responsive contact cards component - Enhance header with mobile responsiveness - Build adaptive footer layout - Ensure cross-device compatibility

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,9 +7,17 @@ import CEOMessage from './Components/About/CEOMessage'
 import FeaturesSection from './Components/Features/FeatureDisplay'
 import ContactForm from './Components/Contact/ContactForm'
 import Footer from './Components/Footer/Footer'
+import HeaderTop from './Components/Header/HeaderTop'
+import Subheader from './Components/Header/Subheader'
 function App() {
   return (
-   
+    <>
+<Home />
+      {/* <HeaderTop /> */}
+      {/* <Subheader /> */}
+    </>
+  
+  //  
   );
 }
 
