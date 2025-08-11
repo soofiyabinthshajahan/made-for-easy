6ed760605e31f488d96b367c14ffc1b429a42ea7
Travel023 <eldhoseer16@gmail.comgit config --global user.email eldhoseer16@gmail.comgit config --global user.name Eldhose>
2025-08-11 20:30:21 +0530

    fix: General page issues

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import './Global.css'
 import Home from './Components/Home/Home'
-import LogoDisplay from './Components/LogoDisplay/LogoDisplay'
-import Presence from './Components/Presence/Presence'
-import CEOMessage from './Components/About/CEOMessage'
-import FeaturesSection from './Components/Features/FeatureDisplay'
-import ContactForm from './Components/Contact/ContactForm'
-import Footer from './Components/Footer/Footer'
+
 function App() {
   return (
    <>
